c6d8f631a046a55e7882a5f3fec572f6107e0590
jjalcazar <jjac89@gmail.com>
2012-01-18 21:25:28 +0100

    añadiendo proyecto

---
+++ AppJam/AppJam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppJam
+//
+//  Created by Juan Jose on 18/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
